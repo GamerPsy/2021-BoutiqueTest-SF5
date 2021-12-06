20ac8919b0616f15c5e6e434f030f8b7f48f4e99
Rémy DUCRY <ducry.remy@gmail.com>
2021-12-06 23:16:23 +0100

    ajout de champs pour le formulaire inscription et réduction largeur du formulaire pour 25%

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,17 +4,53 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-        ;
+            ->add('firstname', TextType::class, [
+                'label' => "Prénom",
+                'attr' => [
+                    'placeholder' => "Votre prénom"
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => "Nom",
+                'attr' => [
+                    'placeholder' => "Votre nom"
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => "Identifiant/E-mail",
+                'attr' => [
+                    'placeholder' => "Votre e-mail"
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => "Mot de passe",
+                'attr' => [
+                    'placeholder' => "Votre mot de passe"
+                ]
+            ])
+            ->add('password_confirm', PasswordType::class, [
+                'label' => "Confirmez votre mot de passe",
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => "Votre mot de passe"
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire"
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
