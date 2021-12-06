657769999492e24148cf96ba5e32b73313ec845e
Rémy DUCRY <ducry.remy@gmail.com>
2021-12-06 22:28:40 +0100

    création du formulaire et vue du formulaire

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegisterType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'register')]
     public function index(): Response
     {
-        return $this->render('register/index.html.twig');
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        return $this->renderForm('register/index.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
