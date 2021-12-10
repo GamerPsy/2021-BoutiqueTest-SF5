5104a3405ecce15ca1b790f1e6e0ec2046b0132d
Rémy DUCRY <ducry.remy@gmail.com>
2021-12-10 19:26:28 +0100

    ajout du hash du password

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
@@ -20,7 +22,7 @@ class RegisterController extends AbstractController
     }
     
     #[Route('/inscription', name: 'register')]
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordHasherInterface $hasher): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -29,6 +31,9 @@ class RegisterController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
+            $password = $hasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+            dd($user);
 
             $this->entityManager->persist($user);
             $this->entityManager->flush();
